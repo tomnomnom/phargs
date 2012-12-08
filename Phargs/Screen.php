e6d25b17e4916890bf8a04846086ba1565e2285b
Tom Hudson <mail@tomnomnom.com>
2012-12-08 01:26:21 +0000

    Colorize method

--- Phargs/Screen.php
+++ Phargs/Screen.php
@@ -2,12 +2,66 @@
 namespace Phargs;
 
 class Screen {
+  protected $colors = [
+    'black'  => 0,
+    'red'    => 1,
+    'green'  => 2,
+    'yellow' => 3,
+    'blue'   => 4,
+    'purple' => 5,
+    'cyan'   => 6,
+    'white'  => 7,
+  ];
+  protected $styles = [
+    'regular'   => 0,
+    'bold'      => 1,
+    'underline' => 4
+  ];
+
   public function __construct(){
     
   }
 
   public function out($msg){
     return fputs(STDOUT, $msg);
+  }
+
+  public function colorize($string, $fg = null, $bg = null, $style = 'regular'){
+    if (!isset($this->colors[$fg])){
+      $fg = null;
+    }
+    if (!isset($this->colors[$bg])){
+      $bg = null;
+    }
+    if (!isset($this->styles[$style])){
+      $style = 'regular';
+    }
+
+    $style = $this->styles[$style];
+
+    if (!is_null($fg)){
+      $fg = $this->colors[$fg];
+      $fgEscape = "\033[{$style};3{$fg}m";
+    } else {
+      $fgEscape = "";
+    }
+
+    if (!is_null($bg)){
+      $bg = $this->colors[$bg];
+      $bgEscape = "\033[4{$bg}m";
+    } else {
+      $bgEscape = "";
+    }
+
+    $resetEscape = "\033[0m";
+
+    return $fgEscape.$bgEscape.$string.$resetEscape;
+  }
+
+  protected function getColorEscape($color){
+    if (!isset($this->colors[$color])) return false;
+    $color = $this->colors[$color];
+    return "\033[0;3{$color}m";
   }
 
   public function err($msg){
@@ -28,7 +82,7 @@ class Screen {
     );
   }
 
-  public function debug(){
+  public function varExport(){
     foreach (func_get_args() as $arg){
       $this->outln(var_export($arg, true));
     }
--- example.php
+++ example.php
@@ -1,15 +1,7 @@
 <?php
 include __DIR__.'/Phargs/Init.php';
 
-$o = new \Phargs\Argument\Orchestrator(
-  new \Phargs\Argument\Parser(),
-  $argv
-);
+$s = new \Phargs\Screen();
 
-$o->expectFlag('-h', 'Display help');
-$o->addFlagAlias('-h', '--help');
-$o->expectParam('-f', 'Filename');
-
-$s = new \Phargs\Output\Std();
-$s->log('Doing some stuff');
-$s->log('Doing some more stuff');
+$s->out($s->colorize('Hello,', 'green', 'purple'));
+$s->out($s->colorize(' World!', 'purple', 'green'));
